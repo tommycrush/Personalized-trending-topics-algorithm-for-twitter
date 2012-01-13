f5376b0bd5a28bbe8983e7424d84432c203b7519
Your Name <thomas.crush@uky.edu>
2012-01-13 14:57:01 -0500

    commented

--- config.php
+++ config.php
@@ -5,10 +5,12 @@
  * A single location to store configuration.
  */
 
+//twitter API vars:
 define('CONSUMER_KEY', 'your key');
 define('CONSUMER_SECRET', 'your secret');
-define('OAUTH_CALLBACK', 'http://toppedin.com/callback.php');
+define('OAUTH_CALLBACK', 'http://yourdomain.com/callback.php');
 
+//lets make a persistant connection to the DB:
 mysql_connect("localhost", "your user", "user pass") or die(mysql_error());
 mysql_select_db("db name")or die(mysql_error());
 
